1be15f61b4dd278677bd8c1ad07c903605f6aac7
ManpreetM1 <manpreet.mundi@hotmail.com>
2021-07-18 22:39:28 -0600

    -Started creating initial main loop and added a few testing statements

--- UserInput.java
+++ UserInput.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class UserInput {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Hello, what would you like to begin with?");
+		String userChoice = scn.nextLine();
+		while(!userChoice.equals("quit")){
+				System.out.println("What would you like to do next?(You can always answer quit to exit!)");
+			 userChoice = scn.nextLine();
+			 System.out.println(userChoice);
+			 
+			
+		}
 	}
 
 }
