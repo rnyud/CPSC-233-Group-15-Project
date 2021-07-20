26a89e734a6cae4c08e4b4efab72601c17cf4ed0
timof <timof@LAPTOP-I3VOCEU2>
2021-07-19 19:54:48 -0600

    Added constructors to Income

--- Income.java
+++ Income.java
@@ -1,6 +1,26 @@
 
 public class Income {
+	int weeklyPay;
+	int savings;
+	public static final int WORKING_WEEKS = 52;
 	
 	
-
+	public Income() {}
+	
+	public Income(int pay) {
+		this.weeklyPay = pay;
+		savings = 0;
+	}
+	
+	public Income(int pay, int saved) {
+		this.weeklyPay = pay;
+		this.savings = saved;
+	}
+	
+	
+	public int AnnualIncome() {
+		int annualPay = weeklyPay*WORKING_WEEKS;
+		
+		return annualPay;
+	}
 }
