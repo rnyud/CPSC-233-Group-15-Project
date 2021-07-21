a4efb6f0651dd90b18182242e0254f0846f27fb9
timof <timof@LAPTOP-I3VOCEU2>
2021-07-21 13:16:31 -0600

    Added ExpenseList

---
+++ ExpenseList.java
@@ -0,0 +1,33 @@
+
+public class ExpenseList {
+	Expenses[] expenseList;
+	//Expense List, contains all of the expenses in a list
+	public ExpenseList(int num) {
+		if(num<1) {
+			num = 1;
+		}
+		expenseList = new Expenses[num];
+	}
+	
+	public void addExpense(Expenses expense) {
+		if(expense!=null) {
+			int index = 0;
+			for(; index < expenseList.length && expenseList[index] != null; index++) {
+			}
+			expenseList[index] = expense;
+		}
+	}
+	
+	public float totalExpense() {
+		float total = 0;
+		for(Expenses expense : getExpenseList()) {
+			total+=expense.getExpenseValue();
+		}
+		return total;
+	}
+
+	Expenses[] getExpenseList() {
+		return expenseList;
+	}
+
+}
