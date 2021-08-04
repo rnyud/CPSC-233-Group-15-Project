c4bc450b204727732df55d4612b163f54bb81c82
ManpreetM1 <87155707+ManpreetM1@users.noreply.github.com>
2021-08-04 01:08:28 -0600

    - Added basic initial view for budgetDisplay and created a test file

---
+++ src/view/TestBudgetDisplay.java
@@ -0,0 +1,22 @@
+package view;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TestBudgetDisplay extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+	    	Parent root = FXMLLoader.load(getClass().getResource("VisualizeBudgetDisplay.fxml"));
+	    	Scene scene = new Scene(root);
+	    	arg0.setScene(scene);
+	    	arg0.show();
+	    
+		
+	}
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
