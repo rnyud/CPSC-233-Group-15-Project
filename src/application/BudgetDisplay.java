4ca4203d76043e82e92ae53a8d032a242a087c54
ManpreetM1 <87155707+ManpreetM1@users.noreply.github.com>
2021-08-04 22:06:48 -0600

    - Added creation of budget object after submitting information
    - Changed pie graph to bar chart
    
    - Added initial error checking for mismatched arguments in Expenses

--- src/application/BudgetDisplay.java
+++ src/application/BudgetDisplay.java
@@ -2,15 +2,21 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.Budget;
 import model.ExpenseList;
+import model.Expenses;
 import model.Income;
 
 public class BudgetDisplay  {
-
+	
+	private Budget userBudget;
+	
 	@FXML
     private TextField incomeText;
 
@@ -21,29 +27,61 @@ public class BudgetDisplay  {
     private Button clearButton;
 
     @FXML
-    private PieChart budgetPie;
-
+    private Label errorLabel;
+    
     @FXML
-    private TextField expenseText;
+    private TextField expenseNameText;
+    
+    @FXML
+    private TextField expenseAmountText;
 
     @FXML
     private TextField savingsText;
 
     @FXML
+    private BarChart<?, ?> budgetGraph;
+    
+    @FXML
     void clearText(ActionEvent event) {
-    	expenseText.clear();
+    	expenseNameText.clear();
+    	expenseAmountText.clear();
     	incomeText.clear();
     	savingsText.clear();
+    }
+    @FXML
+    void submitText(ActionEvent event) {
+    	// Needs some work (May need to redo how Expenses in budgetDisplay work/ are inputted)
+    	Income userIncome = new Income(Float.parseFloat(incomeText.getText()),Float.parseFloat(savingsText.getText()));
+    	String[] expenseNames = expenseNameText.getText().split(",");
+    	String[] expenseAmount = expenseAmountText.getText().split(",");
+    	// Check to see if names and amounts are equivalent
+    	if(expenseNames.length != expenseAmount.length) {
+    		errorLabel.setText("Error: Number of expense Names and Number of expense Amounts don't match");
+    		errorLabel.setVisible(true);
+    		clearText(null);
+    	}
+    	ExpenseList userExpenses = new ExpenseList(expenseAmount.length);
+    	for(int i = 0; i < expenseNames.length; i++) {
+    		Expenses expense = new Expenses(expenseNames[i],Float.parseFloat(expenseAmount[i]));
+    		userExpenses.addExpense(expense);
+    	}
+    	Budget userBudget = new Budget(userExpenses,userIncome);
+    
+    	
     }
     
     @FXML
     void initialize() {
-        assert incomeText != null : "fx:id=\"incomeText\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
+    	assert incomeText != null : "fx:id=\"incomeText\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
+        assert expenseAmountText != null : "fx:id=\"expenseAmountText\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
         assert submitButton != null : "fx:id=\"submitButton\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
+        assert expenseNameText != null : "fx:id=\"expenseNameText\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
         assert clearButton != null : "fx:id=\"clearButton\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
-        assert budgetPie != null : "fx:id=\"budgetPie\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
-        assert expenseText != null : "fx:id=\"expenseText\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
         assert savingsText != null : "fx:id=\"savingsText\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
+        assert budgetGraph != null : "fx:id=\"budgetGraph\" was not injected: check your FXML file 'VisualizeBudgetDisplay.fxml'.";
+           
+        budgetGraph.getXAxis().setLabel("time");
+        budgetGraph.getYAxis().setLabel("time");
 
     }
 }
