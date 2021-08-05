4209d0254ce142afb059c1fc68f39f1247bb58e6
tim-lerm <timofei.lerman@ucalgary.ca>
2021-08-04 22:01:57 -0600

    Considering additional functionality with added bar graph.

--- src/application/ExpenseChartController.java
+++ src/application/ExpenseChartController.java
@@ -1,5 +1,8 @@
 package application;
-
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,7 +12,11 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import model.ExpenseList;
 import model.Expenses;
-
+/**
+ * 
+ * @author timof
+ *
+ */
 public class ExpenseChartController {
 
     @FXML
@@ -21,14 +28,21 @@ public class ExpenseChartController {
     @FXML
     private PieChart expenseChart;
     
-    private ExpenseList list;
+    private ExpenseList list = new ExpenseList(2);
+    
+    private boolean set = false;
     
     public void displayChart() {
+    	
     	PieChart.Data[] expenseData = new PieChart.Data[list.getExpenseList().length];
     	for(int i = 0; i<list.getExpenseList().length; i++) {
     		Expenses expense = list.getExpenseList()[i];
-    		new PieChart.Data(expense.getExpenseName(), expense.getExpenseValue());
+    		if(list.getExpenseList()[i]!=null) {
+	    		System.out.println(expense.getExpenseName());
+	    		expenseData[i] = new PieChart.Data(expense.getExpenseName(), expense.getExpenseValue());
+    		}
     	}
+    	expenseChart.setData(FXCollections.observableArrayList(expenseData));
     	
     	
     }
@@ -36,17 +50,17 @@ public class ExpenseChartController {
     
    public void setList(ExpenseList list) {
 	   this.list = list;
+	   set = true;
+	   displayChart();
    }
 
     @FXML
     void initialize() {
+    	System.out.println("A");
         assert expenseChart != null : "fx:id=\"expenseChart\" was not injected: check your FXML file 'ExpenseChart.fxml'.";
-        Expenses blah = (new Expenses("A", 1));
-    	Expenses blah2 = (new Expenses("B", 1));
-    	ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                new PieChart.Data(blah.getExpenseName(), blah.getExpenseValue()));
-    	expenseChart.setData(FXCollections.observableArrayList(pieChartData));
-        displayChart();
+        
+    	
+    	
+        
     }
 }
--- src/view/TestExpenseChart.java
+++ src/view/TestExpenseChart.java
@@ -11,11 +11,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.ExpenseList;
 import model.Expenses;
-
 public class TestExpenseChart extends Application {
 	public void start(Stage arg0) throws Exception {
 			FXMLLoader loader = new FXMLLoader();
-	    	Parent root = (Parent) loader.load(new FileInputStream("src/view/ExpenseChart.fxml"));
+			Parent root = (Parent) loader.load(new FileInputStream("src/view/ExpenseChart.fxml"));
+			ExpenseChartController chartController = loader.getController();
+			ExpenseList list = new ExpenseList(3);
+			Expenses blah = (new Expenses("Albertan Beef", 100));
+	    	Expenses blah2 = (new Expenses("Baseball", 115));
+	    	Expenses blah3 = (new Expenses("Crickets", 378));
+	    	list.addExpense(blah);
+	    	list.addExpense(blah2);
+	    	list.addExpense(blah3);
+			chartController.setList(list);
 	    	Scene scene = new Scene(root);
 	    	arg0.setScene(scene);
 	    	arg0.show();
